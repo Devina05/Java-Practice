e3451fe303f56826c42dc54459aa38cd99895c08
Devina Gandhi <156473392+Devina05@users.noreply.github.com>
2025-10-02 01:28:52 +0530

    Add files via upload

---
+++ Acess.java
@@ -0,0 +1,12 @@
+public class Acess {
+  public static void main(String[] args) {
+    Student s1 = new Student();
+    System.out.println(s1.name);
+    System.out.println(s1.Rollno);
+  }
+}
+
+class  Student {
+    String name = "Chandrakant";
+    int Rollno = 123;
+}
---
+++ BinarySearch.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class BinarySearch
+{
+    public static void main( String args [])
+    {
+        int counter, num, item, first, last, middle;
+        Scanner input = new Scanner( System.in);
+        System.out.println("enter no of elements");
+        num=input.nextInt();
+        int arr[]=new int[num];
+        System.out.println("Enter " + num + " integers");
+        for (counter = 0; counter < num; counter++)
+        arr[counter] = input.nextInt();
+         System.out.println("Enter the search value:");
+         item = input.nextInt();
+         first = 0;
+         last = num - 1;
+         middle = (first + last)/2;
+         while( first <= last )
+		{
+         if ( arr[middle] < item )
+		 {first = middle + 1;
+	 }
+         else if ( arr[middle] == item )
+         {
+           System.out.println(item + " found at location " + (middle + 1) + ".");
+           break;
+         }
+         else
+         {
+             last = middle - 1;
+         }
+         middle = (first + last)/2;
+		}
+      if ( first > last )
+	  { System.out.println(item + " is not found.\n");
+	  }
+   }
+}
